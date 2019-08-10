e10cf877a97d7d12f95dbeb60d5c714533d23bdf
glqdlt <dlfdnd0725@gmail.com>
2019-08-10 21:06:06 +0900

    웹소켓 푸시 서버 추가

---
+++ websocks-push/src/main/java/com/glqdlt/pm6/websockspush/WebsocksPushApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.pm6.websockspush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocksPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocksPushApplication.class, args);
+    }
+
+}
