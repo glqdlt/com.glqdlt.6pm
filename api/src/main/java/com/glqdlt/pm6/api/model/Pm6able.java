e63b03abe508b2511b62e1bd88d42d52303cd0ae
glqdlt <dlfdnd0725@gmail.com>
2019-11-10 11:51:40 +0900

    Pm6able 에 getRegDate 메소드를 정의

--- api/src/main/java/com/glqdlt/pm6/api/model/Pm6able.java
+++ api/src/main/java/com/glqdlt/pm6/api/model/Pm6able.java
@@ -1,9 +1,12 @@
 package com.glqdlt.pm6.api.model;
 
+import java.time.LocalDateTime;
+
 /**
  * Date 2019-11-10
  *
  * @author glqdlt
  */
 public interface Pm6able extends UniqueNumber {
+    LocalDateTime getRegDate();
 }
