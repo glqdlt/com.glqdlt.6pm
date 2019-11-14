c7e42725bf4529badf845c1a0c941775b7a39be7
glqdlt <dlfdnd0725@gmail.com>
2019-11-14 21:12:00 +0900

    당장 필요하지 않은 하위 프로젝트 삭제

--- entity/src/main/java/com/glqdlt/pm6/entity/upload/AuthorEmbeddedThumbnailImage.java
+++
@@ -1,9 +0,0 @@
-package com.glqdlt.pm6.entity.upload;
-
-/**
- * Date 2019-08-17
- *
- * @author glqdlt
- */
-public class AuthorEmbeddedThumbnailImage extends EmbeddedThumbnailImage implements ThumbnailImageType.AuthorThumbnailImage {
-}
